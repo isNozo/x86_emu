d97806db6fb4e5bbdd886aa5076d805b6362aec5
Nozo <nozomi.pine@gmail.com>
2021-01-13 11:12:03 +0000

    Implement reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use std::env;
+use std::process;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
+    // read cmdline args
     let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: px86 filename");
+        process::exit(1);
+    }
     let filename = &args[1];
 
-    println!("filename = {}", filename);
+    // open file
+    let mut file = File::open(filename)
+        .expect("file not found");
+
+    // read file
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("Contents:\n{}", contents);
 }
