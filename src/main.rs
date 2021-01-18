999f8ff410fce329554d3a692a3ab73e9c82ab12
Nozo <nozomi.pine@gmail.com>
2021-01-18 07:06:19 +0000

    Add struct of Emulator

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
+#![allow(dead_code)]
+
 use std::env;
 use std::process;
 use std::fs::File;
 use std::io::prelude::*;
+use Register::*;
+
+// starts at 0 (EAX=0)
+enum Register { EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI, RegisterCount }
+
+struct Emulator {
+    // General-purpose Registers
+    registers: [u32; RegisterCount as usize],
+    // EFLAGS Register
+    eflags: u32,
+    // Instruction Pointer
+    eip: u32,
+    // Memory
+    memory: Vec<u8>
+}
+
+fn create_emu() -> Emulator {
+    // create new instance
+    Emulator {
+        registers: [0; RegisterCount as usize],
+        eflags: 0,
+        eip: 0,
+        memory: Vec::new()
+    }
+}
 
 fn main() {
     // read cmdline args
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("usage: px86 filename");
+        println!("usage: x86_emu filename");
         process::exit(1);
     }
     let filename = &args[1];
@@ -17,9 +44,9 @@ fn main() {
         .expect("file not found");
 
     // read file as byte
-    let mut memory: Vec<u8> = Vec::new();
-    file.read_to_end(&mut memory)
+    let mut emu = create_emu();
+    file.read_to_end(&mut emu.memory)
         .expect("something went wrong reading the file");
 
-    println!("Contents:\n {:02x?}", &memory);
+    println!("Contents:\n {:02x?}", &emu.memory);
 }
