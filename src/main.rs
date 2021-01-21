8ab429db5adba6ee82ff63a59d297baad5f0baba
Nozo <nozomi.pine@gmail.com>
2021-01-21 02:20:06 +0000

    Add emulation loop

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::prelude::*;
 use Register::*;
 
 // General-purpose Registers Set
-// This enum is used as index to access registers[] field of Emulator
+// This enum is used as index to access the registers[] field of Emulator
 enum Register { EAX=0, ECX, EDX, EBX, ESP, EBP, ESI, EDI }
 const REGISTERS_COUNT: usize = 8;
 const REGISTERS_NAME: [&str; REGISTERS_COUNT] = ["EAX", "ECX", "EDX", "EBX", "ESP", "EBP", "ESI", "EDI"];
@@ -23,7 +23,7 @@ struct Emulator {
     memory: Vec<u8>
 }
 
-// Create new instance of emulator with EIP and ESP
+// Create a new instance of the emulator with EIP and ESP
 fn create_emu(eip: u32, esp: u32) -> Emulator {
     let mut emu = Emulator {
         // Clear all resisters by 0
@@ -42,6 +42,11 @@ fn create_emu(eip: u32, esp: u32) -> Emulator {
     emu
 }
 
+// Read a byte code from a emulator's memory
+fn get_code8(emu: &Emulator, offset: usize) -> u8 {
+    emu.memory[emu.eip as usize + offset]
+}
+
 // Dump general-purpose registers and EIP values
 fn dump_registers(emu: &Emulator) {
     for i in 0..REGISTERS_COUNT {
@@ -51,7 +56,7 @@ fn dump_registers(emu: &Emulator) {
 }
 
 fn main() {
-    // Read filename from command args
+    // Read a filename from command argments
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("usage: x86_emu filename");
@@ -59,17 +64,30 @@ fn main() {
     }
     let filename = &args[1];
 
-    // Open binary file
+    // Open a binary file that contains a x86 machine code
     let mut file = File::open(filename)
         .expect("file not found");
 
-    // Create emulator with EIP=0x0000 and ESP=0x7c00
-    let mut emu = create_emu(0x0000, 0x7c00);
+    // Create a emulator with EIP=0x0000_0000 and ESP=0x0000_7c00
+    let mut emu = create_emu(0x0000_0000, 0x0000_7c00);
     
-    // Read binary file into memory
+    // Load the binary file into the emulator's memory
     file.read_to_end(&mut emu.memory)
         .expect("something went wrong reading the file");
 
-    // Stop program and dump registers
+    // Emulation loop
+    loop {
+        // Read a instruction code
+        let code: u8 = get_code8(&emu, 0);
+        println!("EIP = {:#010x}, Code = {:#04x}", emu.eip, code);
+
+        // Stop the program when EIP=0
+        if emu.eip == 0x0000_0000 {
+            println!("\nend of program.\n");
+            break;
+        }
+    }
+
+    // Stop the program and dump registers
     dump_registers(&emu);
 }
